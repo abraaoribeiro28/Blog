9d5c7893381f40f4ffa3c00c41b3fb07ec0b3b99
abraao <abraao@pbsoft.com.br>
2023-10-07 21:10:52 -0300

    Revert "feat: instalando filamentphp e removendo breeze"
    This reverts commit ebe59b6142682d67f7e7f42f724fc318d38ed0b3.

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                "resources/css/tailwind.css",
                 "resources/css/app.css",
                 "resources/css/home.css",
                 "resources/js/app.js",
