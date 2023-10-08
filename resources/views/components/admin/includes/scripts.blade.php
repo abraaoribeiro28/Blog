430f0f6969180052dca1ebcd98bf4cf86680561c
abraao <abraao@pbsoft.com.br>
2023-10-07 23:00:32 -0300

    feat: instalando tema, configurando layout e componentizando includes

---
+++ resources/views/components/admin/includes/scripts.blade.php
@@ -0,0 +1,6 @@
+<script src="{{ url('theme/src/assets/js/bundle.js') }}"></script>
+<script src="{{ url('theme/src/assets/js/scripts.js') }}"></script>
+
+@vite(['resources/js/app.js'])
+
+@yield('script')
