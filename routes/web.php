5414e0316b37ef117f9614f0924645db306a660e
abraao <abraaobeiro@gmail.com>
2024-03-29 14:45:09 -0300

    Recriando arquivos de rotas deletados

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+
+use App\Http\Controllers\Portal\HomeController;
+use App\Http\Controllers\Portal\PostController;
+use App\Http\Controllers\Portal\SubscriberController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', HomeController::class)->name('portal.home');
+
+// Postagens
+Route::get('posts/{slug}', [PostController::class, 'show'])->name('posts.show');
+
+// Subscriber
+Route::post('ajax-subscribe', [SubscriberController::class, 'store'])->name('ajax.subscribe');
+
+require __DIR__.'/auth.php';
+
+//Route::get('/send', function () {
+//    $post = \App\Models\Admin\Post::first();
+//   \App\Jobs\SendEmailsJob::dispatch($post);
+//});
