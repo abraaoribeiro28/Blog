dd9e2f0613c71f698a1f0e3cd351a5265702db34
Abraão <abraaobeiro@gmail.com>
2024-03-31 18:00:48 -0300

    Ajusta responsividade da lista de posts

--- resources/views/portal/pages/post/index.blade.php
+++ resources/views/portal/pages/post/index.blade.php
@@ -3,15 +3,15 @@
         <div class="container">
             <div class="row">
                 @foreach($posts as $post)
-                    <div class="col-md-4">
+                    <div class="col-md-4 col-sm-6">
                     <div class="blog-index w-dyn-list">
-                        <a href="{{ route('posts.show', $post->slug) }}" class="post-recente d-block d-sm-grid align-items-start">
+                        <a href="{{ route('posts.show', $post->slug) }}" class="post-recente d-grid align-items-start">
                             <img src="{{ getPathStorage($post->highlightArchive->path ?? '#') }}" class="imagem-palestra-recentes" alt="imagem de palestra" loading="lazy" />
             
                             <div style="line-height: normal;">
                                 <div class="category">{{ $post->category->name }}</div>
                                 <h4 class="line-4 mb-0">{{ $post->title }}</h4>
-                                <div class="post-details d-lg-none">
+                                <div class="post-details d-none">
                                     <div>{{ $post->author }}</div>
                                     <div class="spacer-dot">•</div>
                                     <div>{{ formatDateWithMonth($post->publication_date) }}</div>
@@ -34,9 +34,35 @@
                 grid-column-gap: 5px;
             }
 
-            @media screen and (max-width: 1399px){
-                .imagem-palestra-recentes {
-                    height: 117px;
+            .imagem-palestra-recentes {
+                height: 117px;
+            }
+
+            @media screen and (min-width: 992px){
+                .post-recente {
+                    grid-template-columns: 1fr 1.5fr;
+                }
+            }
+
+            @media screen and (min-width: 1200px){
+                .post-recente {
+                    grid-template-columns: 1fr 2fr;
+                }
+            }
+
+            @media screen and (max-width: 575px){
+                .post-recente h4{
+                    margin: 5px 0 !important;
+                }
+
+                .post-recente {
+                    grid-template-columns: 1fr 2fr;
+                }
+            }
+
+            @media screen and (max-width: 425px){
+                .post-recente {
+                    grid-template-columns: 1fr 2fr;
                 }
             }
         </style>
