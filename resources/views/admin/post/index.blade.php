26b4ad0738aa34c66dd0d6906226a021b784718a
Abraão <abraaobeiro@gmail.com>
2024-03-31 22:10:32 -0300

    Melhora a responsividade da listagem dos posts

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -2,22 +2,23 @@
     <div class="nk-content-wrap">
         <div class="nk-block nk-block-lg">
             <div class="nk-block-head nk-block-head-sm">
-                <div class="nk-block-between">
+                <div class="nk-block-between align-items-start align-items-md-center flex-column flex-md-row">
                     <div class="nk-block-head-content">
                         <h3 class="nk-block-title page-title">Postagens</h3>
                         <div class="nk-block-des text-soft">
                             <p>Listagem dos registros de postagrens.</p>
                         </div>
                     </div>
-                    <div class="nk-block-head-content">
+                    <div class="nk-block-head-content mt-2 mt-md-0">
                         <div class="toggle-wrap nk-block-tools-toggle">
                             @can('postagens.create')
-                                <a href="{{ route('categories.index') }}" class="btn btn-info">
-                                    Categoria de postagens
-                                </a>
                                 <a href="{{ route('posts.create') }}" class="btn btn-primary">
                                     <i class="icon bi bi-plus me-1"></i>
-                                   Nova postagem
+                                   Cadastrar
+                                </a>
+                                <a href="{{ route('categories.index') }}" class="btn btn-secondary">
+                                    <i class="icon bi bi-tags me-1"></i>
+                                    Categorias
                                 </a>
                             @endcan
                         </div>
@@ -26,59 +27,46 @@
                 <x-admin.forms.alert/>
             </div>
 
-
             <div class="card card-bordered card-preview">
                 <div class="card-inner">
                     <table class="datatable-init nk-tb-list nk-tb-ulist" data-auto-responsive="false">
                         <thead>
                             <tr class="nk-tb-item nk-tb-head">
-                                <th class="nk-tb-col nk-tb-col-check">
-                                    <div class="custom-control custom-control-sm custom-checkbox notext">
-                                        <input type="checkbox" class="custom-control-input" id="uid">
-                                        <label class="custom-control-label" for="uid"></label>
-                                    </div>
-                                </th>
                                 <th class="nk-tb-col"><span class="sub-text">Título</span></th>
-                                <th class="nk-tb-col tb-col-mb"><span class="sub-text">Categoria</span></th>
-                                <th class="nk-tb-col tb-col-md"><span class="sub-text">Data de publicação</span></th>
-                                <th class="nk-tb-col tb-col-md"><span class="sub-text">Status</span></th>
+                                <th class="nk-tb-col"><span class="sub-text">Categoria</span></th>
+                                <th class="nk-tb-col"><span class="sub-text">Data de publicação</span></th>
+                                <th class="nk-tb-col"><span class="sub-text">Status</span></th>
                                 @canany(['postagens.edit', 'postagens.destroy'])
-                                    <th class="nk-tb-col tb-col-md nk-tb-col-tools text-center">Ações</th>
+                                    <th class="nk-tb-col text-center">Ações</th>
                                 @endcanany
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($posts as $post)
                                 <tr class="nk-tb-item" id="item-{{$post->id}}">
-                                    <td class="nk-tb-col nk-tb-col-check">
-                                        <div class="custom-control custom-control-sm custom-checkbox notext">
-                                            <input type="checkbox" class="custom-control-input" id="{{ $post->id }}">
-                                            <label class="custom-control-label" for="{{ $post->id }}"></label>
-                                        </div>
-                                    </td>
                                     <td class="nk-tb-col">
                                         <span class="tb-lead">{{ Str::limit($post->title, 40, '...') }}</span>
                                     </td>
-                                    <td class="nk-tb-col tb-col-mb">
+                                    <td class="nk-tb-col">
                                         <span class="tb-lead">{{ $post->category->name }}</span>
                                     </td>
-                                    <td class="nk-tb-col tb-col-md">
+                                    <td class="nk-tb-col">
                                         <span>{{ date('d/m/Y', strtotime($post->publication_date)) }}</span>
                                     </td>
 
-                                    <td class="nk-tb-col tb-col-md">
-                                        <span class="tb-status">
+                                    <td class="nk-tb-col">
+                                        <span class="tb-status d-flex align-items-center">
                                             @if($post->status)
-                                                <em class="icon ni ni-check-circle text-success"></em>
+                                                <em class="icon ni ni-check-circle text-success me-1"></em>
                                                 Publicado
                                             @else
-                                                <em class="icon ni ni-cross-circle text-danger"></em>
+                                                <em class="icon ni ni-cross-circle text-danger me-1"></em>
                                                 Rascunho
                                             @endif
                                         </span>
                                     </td>
                                     @canany(['postagens.edit', 'postagens.destroy'])
-                                        <td class="nk-tb-col tb-col-md text-center">
+                                        <td class="nk-tb-col text-center">
                                         <div class="dropdown">
                                             <a class="text-soft dropdown-toggle btn btn-icon btn-trigger" data-bs-toggle="dropdown" data-offset="-8,0" aria-expanded="false">
                                                 <em class="icon ni ni-more-h"></em>
