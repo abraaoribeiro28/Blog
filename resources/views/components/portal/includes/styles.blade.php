05cb9f5c7b956e45dab6f9c8decdeb7c59838cea
Abraão Ribeiro <abraaobeiro@gmail.com>
2024-06-01 16:36:44 -0300

    Implementa novo layout da listagem de posts com tailwind

--- resources/views/components/portal/includes/styles.blade.php
+++ resources/views/components/portal/includes/styles.blade.php
@@ -2,12 +2,6 @@
 
 <link rel="icon" type="image/x-icon" href="{{ url($configuration['favicon']) }}">
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:100,200,300,regular,500,600,700,800,900" media="all">
-
-<!-- StyleSheets  -->
-{{--<link rel="stylesheet" href="{{ url('theme/src/assets/css/dashlite.min.css') }}">--}}
-{{--<link id="skin-default" rel="stylesheet" href="{{ url('theme/src/assets/css/theme.css') }}">--}}
-
 <link rel="stylesheet" href="{{ asset($cssColorPath) }}">
 
 @vite(['resources/css/portal/app.css'])
