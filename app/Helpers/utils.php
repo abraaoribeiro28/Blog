d6c56bbfd6d28bbbac456edd7bdc1b74898abbf2
Abraão Ribeiro <abraaobeiro@gmail.com>
2024-06-04 14:15:34 -0300

    Implementa função para pegar o as inciais do usuário

--- app/Helpers/utils.php
+++ app/Helpers/utils.php
@@ -91,3 +91,21 @@ function addStoragePath(string $path): string
 {
     return "storage/$path";
 }
+
+
+/**
+ * Obtém as iniciais do nome do usuário autenticado.
+ *
+ * @return string
+ */
+if (! function_exists('getUsernameInitials')) {
+    function getUsernameInitials(): string
+    {
+        $parts = explode(' ', auth()->user()->name);
+
+        $initials = $parts[0][0] ?? '';
+        $initials .= $parts[1][0] ?? '';
+
+        return strtoupper($initials);
+    }
+}
