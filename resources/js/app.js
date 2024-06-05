3bb11386799458225d9c023e1a03b75478034701
Abraão Ribeiro <abraaobeiro@gmail.com>
2024-06-05 11:25:47 -0300

    Instala sweetalert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,9 @@ import './admin/fixed-plugin.js';
 import './admin/sidebar/sidenav.js';
 import './admin/sidebar/sidenav-burger.js';
 
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+
 import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
-
 Alpine.start();
