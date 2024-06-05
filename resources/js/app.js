22d201ac00fee80096cf0e6a9fdba7f6860b52c5
Abraão Ribeiro <abraaobeiro@gmail.com>
2024-06-05 11:27:48 -0300

    Removendo pacotes do perfect-scrollbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,3 @@
-import './admin/perfect-scrollbar/perfect-scrollbar.min.js';
-import './admin/perfect-scrollbar/perfect-scrollbar.js';
 import './admin/fixed-plugin.js';
 import './admin/sidebar/sidenav.js';
 import './admin/sidebar/sidenav-burger.js';
